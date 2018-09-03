ec64ffd9613fb8c0a21b1bf55eae5c66a6912b8f
Luis Muñoz <github@lem.click>
2018-09-03 13:50:07 -0700

    Backport multi-ns validation, syslogging

--- nsupdate-hook.sh
+++ nsupdate-hook.sh
@@ -13,12 +13,17 @@ PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
 DIG=${DIG:=/usr/bin/dig}
 GREP=${GREP:=/bin/grep}
-MASTER=${MASTER:=}
-NSUPDATE_OPTS=${NSUPDATE_OPTS:=}
+LOGGER=${LOGGER:=/usr/bin/logger}
 NSUPDATE=${NSUPDATE:=`which nsupdate`}
-TSIGKEYFILE=${TSIGKEYFILE:=~/mykey.conf}
+
+LOGGER_OPTS=${LOGGER_OPTS:=-s -p local3.info -t nsupdate-hook}
+NSUPDATE_OPTS=${NSUPDATE_OPTS:=}
+
 FOREIGNNS=${FOREIGNNS:=8.8.8.8}
-TTL=${TTL:=5}
+MASTER=${MASTER:=}
+SLEEPSECS=${SLEEPSECS:=5}
+TSIGKEYFILE=${TSIGKEYFILE:=~/mykey.conf}
+TTL=${TTL:=2}
 
 # Prerequisite checking code below
 
@@ -34,7 +39,7 @@ function check_prerreq {
     if [ "${msg}" = "" ]; then
       echo "  Please install ${binary} or provide its location via environment variables"
     else
-      echo "  " ${msg}
+      echo "  ${msg}"
     fi
 
     missing=1
@@ -66,51 +71,85 @@ fi
 challenge="_acme-challenge.${CERTBOT_DOMAIN}"
 
 function perform_cleanup {
+  ${LOGGER} ${LOGGER_OPTS} "cleaunp ${challenge} ${CERTBOT_VALIDATION}"
   if (  [ "${MASTER}" == "" ] || echo "server ${MASTER}";
-    echo "update delete ${challenge} ${TXT}";
+    echo "update delete ${challenge} TXT ${CERTBOT_VALIDATION}";
     echo send
-  ) | "${NSUPDATE}" -k "${TSIGKEYFILE}" ${NSUPDATE_OPTS}
+  ) | ${NSUPDATE} -k "${TSIGKEYFILE}" ${NSUPDATE_OPTS}
   then
+    ${LOGGER} ${LOGGER_OPTS} "removal of ${challenge} ${CERTBOT_VALIDATION} successful"
     if [ "${VERBOSE}" == "" ]; then
       echo Removed challenge ${challenge}
     fi
   else
+    ${LOGGER} ${LOGGER_OPTS} "failed removal of ${challenge} ${CERTBOT_VALIDATION}"
     echo Failed to remove challenge ${challenge}
   fi
 }
 
+function verify_authorization {
+  domain=$1
+  token=$2
+  challenge="_acme-challenge.${domain}"
+
+  nslist="/tmp/nslist-$$"
+  rm -f "${nslist}"
+
+  [ "${FOREIGNNS}" == "" ] || ( echo "${FOREIGNNS}" > ${nslist} )
+
+  ${DIG} +short NS "${domain}" >> "${nslist}"
+
+  for ns in `cat "${nslist}"`; do
+    if ${DIG} +short IN TXT ${challenge} @${ns} | ${GREP} -F "${token}" > /dev/null
+    then
+      if [ "${VERBOSE}" == "" ]; then
+        ${LOGGER} ${LOGGER_OPTS} "validation of ${challenge} ${token} via ${ns} successful"
+        echo "Validation of ${challenge} via ${ns} was successful"
+      fi
+    else
+      ${LOGGER} ${LOGGER_OPTS} "failed validation of ${challenge} ${token} via ${ns}"
+      echo "Failed validation for ${challenge} -- retrying"
+      rm -f "${nslist}"
+      sleep 1
+      return 255
+    fi
+  done
+
+  rm -f "${nslist}"
+  return 0
+}
+
 function perform_authorization {
+  if [ "${MASTER}" == "" ]; then
+    ${LOGGER} ${LOGGER_OPTS} "initialize challenge ${challenge} ${CERTBOT_VALIDATION}"
+  else
+    ${LOGGER} ${LOGGER_OPTS} "initialize challenge ${challenge} ${CERTBOT_VALIDATION} with master ${MASTER}"
+  fi
+
   if (
     [ "${MASTER}" == "" ] || echo "server ${MASTER}";
     echo "update add ${challenge} ${TTL} TXT ${CERTBOT_VALIDATION}";
     echo send
   ) | "${NSUPDATE}" -k "${TSIGKEYFILE}" ${NSUPDATE_OPTS}
   then
+    ${LOGGER} ${LOGGER_OPTS} "challenge ${challenge} ${CERTBOT_VALIDATION} addedd successfully"
     if [ "${VERBOSE}" == "" ]; then
       echo Added challenge ${challenge}
     fi
 
-    while :; do
-      if ${DIG} +short IN TXT ${challenge} @${FOREIGNNS} | ${GREP} "${CERTBOT_VALIDATION}" > /dev/null; then
-        if [ "${VERBOSE}" == "" ]; then
-          echo "Validation of ${challenge} was successful"
-        fi
-        return
-      else
-        echo "Failed validation for ${challenge} -- retrying"
-        sleep 1;
-      fi
+    while ! verify_authorization "${CERTBOT_DOMAIN}" "${CERTBOT_VALIDATION}"; do
+      ${LOGGER} ${LOGGER_OPTS} "retry verification of ${challenge} ${CERTBOT_VALIDATION}"
+      sleep ${SLEEPSECS};
     done
 
   else
+    ${LOGGER} ${LOGGER_OPTS} "failed to add challenge ${challenge} ${CERTBOT_VALIDATION}"
     echo Failed to add challenge ${challenge}
     exit 255
   fi
 }
 
-if [ "${VERBOSE}" == "" ]; then
-  echo "nsupdate-hook: domain=${CERTBOT_DOMAIN} validation=${CERTBOT_VALIDATION}"
-fi
+${LOGGER} ${LOGGER_OPTS} "domain=${CERTBOT_DOMAIN} validation=${CERTBOT_VALIDATION}"
 
 if [ "${CERTBOT_AUTH_OUTPUT}" == "" ]; then
   perform_authorization
